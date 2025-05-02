9e012f0f3a21137a1400f88c931cd1a4c633370a
Valentin Popescu <valentinpopescu98@gmail.com>
2025-05-02 13:49:13 +0300

    Initial configuration

---
+++ src/main/java/com/valentin/demo_aws/DemoAwsApplication.java
@@ -0,0 +1,13 @@
+package com.valentin.demo_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valentin/demo_aws/DemoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentin.demo_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
