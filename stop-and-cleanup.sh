8910c97615411255317e7786ed41a9679e6c2808
Valentin Popescu <67784803+valentinpopescu98@users.noreply.github.com>
2025-05-10 14:29:31 +0300

    Update stop-and-cleanup.sh

--- stop-and-cleanup.sh
+++ stop-and-cleanup.sh
@@ -7,3 +7,5 @@ CONTAINER_NAME="${APP_NAME}-container"
 # Stop & remove previous container if it exists
 echo "Cleaning old container if exists..."
 docker rm -f $CONTAINER_NAME 2>/dev/null || true
+
+echo "Cleanup completed for $CONTAINER_NAME"
