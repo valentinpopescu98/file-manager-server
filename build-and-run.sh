9c123b94140d8d2fb2acf9e7ab6cbbb7821a63a2
Valentin Popescu <valentinpopescu98@gmail.com>
2025-06-19 22:06:37 +0300

    Switch from HTTP to HTTPS

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
+set -eu
 
 # Config
 APP_NAME="file-manager-server"
 CONTAINER_NAME="${APP_NAME}-container"
-IMAGE_NAME="${APP_NAME}-app"
 
 # Go to project directory
 cd /home/ubuntu/$APP_NAME || { echo "Project directory not found"; exit 1; }
 
-# Build
-echo "Building Docker image..."
-docker build -t $IMAGE_NAME .
-
 # Stop & remove previous container if it exists
 echo "Cleaning old container if exists..."
-docker rm -f $CONTAINER_NAME 2>/dev/null || true
+docker-compose down
 
-# Run application in container with port 8080 open
-echo "Starting new container..."
-docker run -d --network host \
-  --name $CONTAINER_NAME \
-  -p 8080:8080 \
-  $IMAGE_NAME
+# Build & Run
+echo "Building and starting Docker image..."
+docker-compose up -d --build
 
 echo "Container $CONTAINER_NAME is now running at port 8080"
--- stop-and-cleanup.sh
+++ stop-and-cleanup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -eu
 
 # Config
 APP_NAME="file-manager-server"
 CONTAINER_NAME="${APP_NAME}-container"
 
+# Go to project directory
+cd /home/ubuntu/$APP_NAME || { echo "Project directory not found"; exit 1; }
+
 # Stop & remove previous container if it exists
 echo "Cleaning old container if exists..."
-docker rm -f $CONTAINER_NAME 2>/dev/null || true
+docker-compose down
 
 echo "Cleanup completed for $CONTAINER_NAME"
